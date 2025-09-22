d09de91bb2aa5926248fa6e7d5baacfb34b59a70
redshadowz <39907945+redshadowz@users.noreply.github.com>
2025-09-22 11:26:18 -0500

    Add files via upload

--- Clean_Up.lua
+++ Clean_Up.lua
@@ -5,8 +5,8 @@ self:SetScript('OnEvent', function() this[event](this) end)
 for _, event in {'ADDON_LOADED', 'PLAYER_LOGIN', 'MERCHANT_SHOW', 'MERCHANT_CLOSED'} do
 	self:RegisterEvent(event)
 end
-local maxMovesPerClick = 5;
-local moveCounter = 0;
+local maxMovesPerClick = 5
+local moveCounter = 0
 local lastTime
 local lastSlotAndLink = {}
 
@@ -41,71 +41,43 @@ function self.ADDON_LOADED()
 	end
 	if Bagnon then Clean_Up_Settings["bags"].parent = "BagnonTitle" Clean_Up_Settings["bags"].position = {0, -1} self:CreateButton'bags' Clean_Up_Settings["bank"].parent = "BanknonTitle" Clean_Up_Settings["bank"].position = {0, -1} self:CreateButton'bank' end
 	self.CLASSES = {
-		-- arrow
-		{
+		{ -- arrow
 			containers = {2101, 5439, 7278, 11362, 3573, 3605, 7371, 8217, 2662, 19319, 18714},
 			items = self:Set(2512, 2515, 3030, 3464, 9399, 11285, 12654, 18042, 19316),
 		},
-		-- bullet
-		{
+		{ -- bullet
 			containers = {2102, 5441, 7279, 11363, 3574, 3604, 7372, 8218, 2663, 19320},
 			items = self:Set(2516, 2519, 3033, 3465, 4960, 5568, 8067, 8068, 8069, 10512, 10513, 11284, 11630, 13377, 15997, 19317),
 		},
-		-- soul
-		{
+		{ -- soul
 			containers = {22243, 22244, 21340, 21341, 21342},
 			items = self:Set(6265),
 		},
-		-- ench
-		{
+		{ -- enchanting
 			containers = {22246, 22248, 22249},
 			items = self:Set(
-				-- dust
-				10940, 11083, 11137, 11176, 16204,
-				-- essence
-				10938, 10939, 10998, 11082, 11134, 11135, 11174, 11175, 16202, 16203,
-				--shard
-				10978, 11084, 11138, 11139, 11177, 11178, 14343, 14344,
-				-- crystal
-				20725,
-				--rod
-				6218, 6339, 11130, 11145, 16207
+				10940, 11083, 11137, 11176, 16204, -- dust
+				10938, 10939, 10998, 11082, 11134, 11135, 11174, 11175, 16202, 16203, -- essence
+				10978, 11084, 11138, 11139, 11177, 11178, 14343, 14344, --shard
+				20725, -- crystal
+				6218, 6339, 11130, 11145, 16207 --rod
 			),
 		},
-		-- herb
-		{
+		{ -- herb
 			containers = {22250, 22251, 22252},
 			items = self:Set(765, 785, 2447, 2449, 2450, 2452, 2453, 3355, 3356, 3357, 3358, 3369, 3818, 3819, 3820, 3821, 4625, 8831, 8836, 8838, 8839, 8845, 8846, 13463, 13464, 13465, 13466, 13467, 13468),
 		},
 	}
-
 	self.MOUNT = self:Set(
-		-- rams
-		5864, 5872, 5873, 18785, 18786, 18787, 18244, 19030, 13328, 13329,
-
-		-- horses
-		2411, 2414, 5655, 5656, 18778, 18776, 18777, 18241, 12353, 12354,
-
-		-- sabers
-		8629, 8631, 8632, 18766, 18767, 18902, 18242, 13086, 19902, 12302, 12303, 8628, 12326,
-
-		-- mechanostriders
-		8563, 8595, 13321, 13322, 18772, 18773, 18774, 18243, 13326, 13327,
-
-		-- kodos
-		15277, 15290, 18793, 18794, 18795, 18247, 15292, 15293,
-
-		-- wolves
-		1132, 5665, 5668, 18796, 18797, 18798, 18245, 12330, 12351,
-
-		-- raptors
-		8588, 8591, 8592, 18788, 18789, 18790, 18246, 19872, 8586, 13317,
-
-		-- undead horses
-		13331, 13332, 13333, 13334, 18791, 18248, 13335,
-
-		-- qiraji battle tanks
-		21218, 21321, 21323, 21324, 21176
+		5864, 5872, 5873, 18785, 18786, 18787, 18244, 19030, 13328, 13329, -- rams
+		2411, 2414, 5655, 5656, 18778, 18776, 18777, 18241, 12353, 12354, -- horses
+		8629, 8631, 8632, 18766, 18767, 18902, 18242, 13086, 19902, 12302, 12303, 8628, 12326, -- sabers
+		8563, 8595, 13321, 13322, 18772, 18773, 18774, 18243, 13326, 13327, -- mechanostriders
+		15277, 15290, 18793, 18794, 18795, 18247, 15292, 15293, -- kodos
+		1132, 5665, 5668, 18796, 18797, 18798, 18245, 12330, 12351, -- wolves
+		8588, 8591, 8592, 18788, 18789, 18790, 18246, 19872, 8586, 13317, -- raptors
+		13331, 13332, 13333, 13334, 18791, 18248, 13335, -- undead horses
+		21218, 21321, 21323, 21324, 21176 -- qiraji battle tanks
 	)
 	self.SPECIAL = self:Set(5462, 17696, 17117, 13347, 13289, 11511)
 	self.KEY = self:Set(9240, 17191, 13544, 12324, 16309, 12384, 20402)
@@ -117,7 +89,7 @@ function self.ADDON_LOADED()
 	self:CreateButton'bags'
 	self:CreateButton'bank'
 end
-local count = 0;
+local count = 0
 function self:PLAYER_LOGIN()
 	self.PickupContainerItem = PickupContainerItem
 	function PickupContainerItem(...)
@@ -153,15 +125,15 @@ function self:PLAYER_LOGIN()
 						if self:SlotKey(container, position) ~= slot and GetContainerItemLink(container, position) == link then
 							arg[1], arg[2] = container, position
 							self.UseContainerItem(unpack(arg))
-							count = count + 1;
-							if self.atMerchant and count == 5 then break; end
+							count = count + 1
+							if self.atMerchant and count == 5 then break end
 						end
 					end
-					if self.atMerchant and count == 5 then break; end
+					if self.atMerchant and count == 5 then break end
 				end
 			end
-			if count == 5 then lastTime = GetTime() - .5; else lastTime = GetTime() end
-			count = 0;
+			if count == 5 then lastTime = GetTime() - .5 else lastTime = GetTime() end
+			count = 0
 			lastSlotAndLink[1] = slot
 			lastSlotAndLink[2] = link
 			self.UseContainerItem(unpack(arg))
@@ -175,7 +147,6 @@ function self:UPDATE()
 	if slowdowntheupdateimer < GetTime() and not IsAltKeyDown() then
 		slowdowntheupdateimer = GetTime() + .5
 		count = 0
-
 		if moveCounter == 0 and keyToRepeatIfNotFinished then self:Go(keyToRepeatIfNotFinished) else keyToRepeatIfNotFinished = nil end
 		if not self.model then
 			self:CreateModel()
@@ -204,9 +175,7 @@ function self:Log(msg)
 end
 function self:Set(...)
 	local t = {}
-	for i=1,arg.n do
-		t[arg[i]] = true
-	end
+	for i=1,arg.n do t[arg[i]] = true end
 	return t
 end
 function self:LT(a, b)
@@ -224,9 +193,7 @@ function self:LT(a, b)
 end
 function self:Key(table, value)
 	for k, v in table do
-		if v == value then
-			return k
-		end
+		if v == value then return k end
 	end
 end
 function self:SlotKey(container, position)
@@ -238,13 +205,11 @@ function self:SetupSlash()
 		self.buttonPlacer.key = 'bags'
 		self.buttonPlacer:Show()
 	end
-
 	SLASH_CLEANUPBANK1 = '/cleanupbank'
 	function SlashCmdList.CLEANUPBANK(arg)
 		self.buttonPlacer.key = 'bank'
 		self.buttonPlacer:Show()
 	end
-
     SLASH_CLEANUPREVERSE1 = '/cleanupreverse'
     function SlashCmdList.CLEANUPREVERSE(arg)
         Clean_Up_Settings.reversed = not Clean_Up_Settings.reversed
@@ -332,9 +297,8 @@ function self:CreateButton(key)
 	end)
 end
 function self:Move(src, dst)
-    local _, _, srcLocked = GetContainerItemInfo(src.container, src.position)
-    local _, _, dstLocked = GetContainerItemInfo(dst.container, dst.position)
-    
+    local _,_,srcLocked = GetContainerItemInfo(src.container, src.position)
+    local _,_,dstLocked = GetContainerItemInfo(dst.container, dst.position)
 	if not srcLocked and not dstLocked then
 		ClearCursor()
        	PickupContainerItem(src.container, src.position)
@@ -347,33 +311,23 @@ function self:Move(src, dst)
 				local count = min(src.state.count, self:Info(dst.state.item).stack - dst.state.count)
 				src.state.count = src.state.count - count
 				dst.state.count = dst.state.count + count
-				if src.count == 0 then
-					src.state.item = nil
-				end
+				if src.count == 0 then src.state.item = nil end
 			else
 				src.state, dst.state = dst.state, src.state
 			end
 		end
-
 		return true
     end
 end
 function self:TooltipInfo(container, position)
 	local chargesPattern = '^'..gsub(gsub(ITEM_SPELL_CHARGES_P1, '%%d', '(%%d+)'), '%%%d+%$d', '(%%d+)')..'$'
-
 	Clean_Up_Tooltip:SetOwner(self, ANCHOR_NONE)
 	Clean_Up_Tooltip:ClearLines()
-
-	if container == BANK_CONTAINER then
-		Clean_Up_Tooltip:SetInventoryItem('player', BankButtonIDToInvSlotID(position))
-	else
-		Clean_Up_Tooltip:SetBagItem(container, position)
-	end
+	if container == BANK_CONTAINER then Clean_Up_Tooltip:SetInventoryItem('player', BankButtonIDToInvSlotID(position)) else Clean_Up_Tooltip:SetBagItem(container, position) end
 
 	local charges, usable, soulbound, quest, conjured
 	for i=1,Clean_Up_Tooltip:NumLines() do
 		local text = getglobal('Clean_Up_TooltipTextLeft'..i):GetText()
-
 		local _, _, chargeString = strfind(text, chargesPattern)
 		if chargeString then
 			charges = tonumber(chargeString)
@@ -387,7 +341,6 @@ function self:TooltipInfo(container, position)
 			conjured = true
 		end
 	end
-
 	return charges or 1, usable, soulbound, quest, conjured
 end
 function self:Sort()
@@ -399,7 +352,6 @@ function self:Sort()
 			if counter == 10 then return false end
 
 			local sources, rank = {}, {}
-
 			for _, src in self.model do
 				if src.state.item == dst.item
 					and src ~= dst
@@ -410,18 +362,15 @@ function self:Sort()
 					tinsert(sources, src)
 				end
 			end
-
 			sort(sources, function(a, b) return rank[a] < rank[b] end)
-
-			for _, src in sources do
-				if self:Move(src, dst) then
+			for _,src in sources do
+				if self:Move(src,dst) then
 					counter=counter+1
 					break
 				end
 			end
 		end
 	end
-
 	return complete
 end
 function self:Stack()
@@ -450,25 +399,19 @@ function self:ClickSell(container,position)
 		end
 	end
 end
-local keyToRepeatIfNotFinished = "bags";
+local keyToRepeatIfNotFinished = "bags"
 function self:Go(key)
 	self.containers = self[key].containers
 	self.model = nil
-	moveCounter = maxMovesPerClick;
-	keyToRepeatIfNotFinished = key;
+	moveCounter = maxMovesPerClick
+	keyToRepeatIfNotFinished = key
 	self:Show()
 end
 do
 	local items, counts
-
 	local function insert(t, v)
-		if Clean_Up_Settings.reversed then
-			tinsert(t, v)
-		else
-			tinsert(t, 1, v)
-		end
+		if Clean_Up_Settings.reversed then tinsert(t, v) else tinsert(t, 1, v) end
 	end
-
 	local function assign(slot, item)
 		if counts[item] > 0 then
 			local count = min(counts[item], self:Info(item).stack)
@@ -478,47 +421,36 @@ do
 			return true
 		end
 	end
-
 	local function assignCustom()
 		for _, slot in self.model do
 			for item in self:Present(Clean_Up_Settings.assignments[self:SlotKey(slot.container, slot.position)]) do
-				if counts[item] then
-					assign(slot, item)
-				end
+				if counts[item] then assign(slot, item) end
 			end
 		end
 	end
-
 	local function assignSpecial()
 		for key, class in self.CLASSES do
 			for _, slot in self.model do
 				if slot.class == key and not slot.item then
 					for _, item in items do
-						if self:Info(item).class == key and assign(slot, item) then
-							break
-						end
+						if self:Info(item).class == key and assign(slot, item) then break end
 				    end
 			    end
 			end
 		end
 	end
-
 	local function assignRemaining()
 		for _, slot in self.model do
 			if not slot.class and not slot.item then
 				for _, item in items do
-					if assign(slot, item) then
-						break
-					end
+					if assign(slot, item) then break end
 			    end
 		    end
 		end
 	end
-
 	function self:CreateModel()
 		self.model = {}
 		counts = {}
-
 		for _, container in self.containers do
 			local class = self:Class(container)
 			for position=1,GetContainerNumSlots(container) do
@@ -535,11 +467,8 @@ do
 			end
 		end
 		items = {}
-		for item, _ in counts do
-			tinsert(items, item)
-		end
+		for item, _ in counts do tinsert(items, item) end
 		sort(items, function(a, b) return self:LT(self:Info(a).sortKey, self:Info(b).sortKey) end)
-
 		assignCustom()
 		assignSpecial()
 		assignRemaining()
@@ -552,9 +481,7 @@ do
 			for name in self:Present(GetBagName(container)) do		
 				for class, info in self.CLASSES do
 					for _, itemID in info.containers do
-						if name == GetItemInfo(itemID) then
-							cache[container] = class
-						end
+						if name == GetItemInfo(itemID) then cache[container] = class end
 					end	
 				end
 			end
@@ -564,78 +491,43 @@ do
 end
 do
 	local cache = {}
-
-	function self:Info(item)
-		return setmetatable({}, {__index=cache[item]})
-	end
-
+	function self:Info(item) return setmetatable({}, {__index=cache[item]}) end
 	function self:Item(container, position)
 		for link in self:Present(GetContainerItemLink(container, position)) do
 			local _, _, itemID, enchantID, suffixID, uniqueID = strfind(link, 'item:(%d+):(%d*):(%d*):(%d*)')
 			itemID = tonumber(itemID)
 			local _, _, quality, _, type, subType, stack, invType = GetItemInfo(itemID)
 			local charges, usable, soulbound, quest, conjured = self:TooltipInfo(container, position)
-
 			local key = format('%s:%s:%s:%s:%s:%s', itemID, enchantID, suffixID, uniqueID, charges, (soulbound and 1 or 0))
-
 			if not cache[key] then
-
 				local sortKey = {}
-
-				-- hearthstone
-				if itemID == 6948 then
+				if itemID == 6948 then -- hearthstone
 					tinsert(sortKey, 1)
-
-				-- mounts
-				elseif self.MOUNT[itemID] then
+				elseif self.MOUNT[itemID] then -- mounts
 					tinsert(sortKey, 2)
-
-				-- special items
-				elseif self.SPECIAL[itemID] then
+				elseif self.SPECIAL[itemID] then -- special items
 					tinsert(sortKey, 3)
-
-				-- key items
-				elseif self.KEY[itemID] then
+				elseif self.KEY[itemID] then -- key items
 					tinsert(sortKey, 4)
-
-				-- tools
-				elseif self.TOOL[itemID] then
+				elseif self.TOOL[itemID] then -- tools
 					tinsert(sortKey, 5)
-
-				-- soulbound items
-				elseif soulbound then
+				elseif soulbound then -- soulbound items
 					tinsert(sortKey, 6)
-
-				-- reagents
-				elseif type == self.ITEM_TYPES[9] then
+				elseif type == self.ITEM_TYPES[9] then -- reagents
 					tinsert(sortKey, 7)
-
-				-- consumables
-				elseif usable and type ~= self.ITEM_TYPES[1] and type ~= self.ITEM_TYPES[2] and type ~= self.ITEM_TYPES[8] or type == self.ITEM_TYPES[4] then
+				elseif usable and type ~= self.ITEM_TYPES[1] and type ~= self.ITEM_TYPES[2] and type ~= self.ITEM_TYPES[8] or type == self.ITEM_TYPES[4] then -- consumables
 					tinsert(sortKey, 8)
-
-				-- quest items
-				elseif quest then
+				elseif quest then -- quest items
 					tinsert(sortKey, 9)
-
-				-- higher quality
-				elseif quality > 1 then
+				elseif quality > 1 then -- higher quality
 					tinsert(sortKey, 10)
-
-				-- common quality
-				elseif quality == 1 then
+				elseif quality == 1 then -- common quality
 					tinsert(sortKey, 11)
-
-				-- conjured items
-				elseif conjured then
+				elseif conjured then -- conjured items
 					tinsert(sortKey, 13)
-				-- junk
-				elseif quality == 0 then
+				elseif quality == 0 then -- junk
 					tinsert(sortKey, 12)
 				end
-
-
-				
 				tinsert(sortKey, self:ItemTypeKey(type))
 				tinsert(sortKey, self:ItemInvTypeKey(type, subType, invType))
 				tinsert(sortKey, self:ItemSubTypeKey(type, subType))
@@ -644,19 +536,11 @@ do
 				tinsert(sortKey, suffixID)
 				tinsert(sortKey, enchantID)
 				tinsert(sortKey, uniqueID)
-
-				cache[key] = {
-					stack = stack,
-					sortKey = sortKey,
-				}
-
+				cache[key] = { stack = stack, sortKey = sortKey, }
 				for class, info in self.CLASSES do
-					if info.items[itemID] then
-						cache[key].class = class
-					end
+					if info.items[itemID] then cache[key].class = class end
 				end
 			end
-
 			return key
 		end
 	end
